76adf3f541513c8c180717a1bc838f4512cd46a6
Rasul Aslanov <rasulsha@code.edu.az>
2024-03-21 18:44:57 +0400

    First

---
+++ ServerSide/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+
+var ip = IPAddress.Broadcast;
+var port = 27001;
+var listener = new UdpClient(port); // Bind to the specified port
+listener.EnableBroadcast = true; // Enable broadcasting
+
+var broadcastEP = new IPEndPoint(ip, port);
+Console.WriteLine($"{broadcastEP} Listener Started...");
+
+while (true)
+{
+    IPEndPoint? remoteEp = null;
+
+    var maxLen = ushort.MaxValue - 29;
+    var len = 0;
+    var buffer = new byte[maxLen];
+    var list = new List<byte>();
+
+    try
+    {
+        // Receive data
+        var result = listener.Receive(ref remoteEp);
+        buffer = result;
+        len = buffer.Length;
+        list.AddRange(buffer);
+
+        var imgBytes = list.ToArray();
+        var chunks = imgBytes?.Chunk(ushort.MaxValue - 29);
+
+        foreach (var chunk in chunks!)
+        {
+            // Send back the received data
+            await listener.SendAsync(chunk, chunk.Length, broadcastEP);
+        }
+
+        list.Clear();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+}
---
+++ ShareScreen/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace ShareScreen
+{
+    public partial class MainWindow : Window
+    {
+        public UdpClient client;
+        public IPEndPoint? remoteEP;
+
+        bool isStart = false;
+        bool isStoped = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            remoteEP = null;
+
+            IPEndPoint test = new IPEndPoint(IPAddress.Any, 27001);
+            client = new UdpClient(test);
+          
+        }
+
+        private async void StartStopClickEvent(object sender, RoutedEventArgs e)
+        {
+            if (!isStart)
+            {
+                isStart = true;
+                StartandStopBTN.Content = "Stop";
+
+                // Start broadcasting
+                await Task.Run(() => StartBroadcasting());
+            }
+            else
+            {
+                isStart = false;
+                StartandStopBTN.Content = "Start";
+                isStoped = true;
+            }
+        }
+
+        private async Task StartBroadcasting()
+        {
+            while (!isStoped)
+            {
+                try
+                {
+                    // Capture the screen
+                    System.Drawing.Image? image = await TakeScreenShotAsync();
+                    byte[]? imageData = await ImageToByteAsync(image);
+                    var chunks = imageData?.Chunk(ushort.MaxValue - 29);
+
+                    // Send the chunks
+                    foreach (var chunk in chunks)
+                    {
+                        await client.SendAsync(chunk, chunk.Length, remoteEP);
+                    }
+
+                    Thread.Sleep(1000);
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public async Task<System.Drawing.Image?> TakeScreenShotAsync()
+        {
+            var width = Screen.PrimaryScreen.Bounds.Width;
+            var height = Screen.PrimaryScreen.Bounds.Height;
+
+            Bitmap bitmap = new Bitmap(width, height);
+
+            using Graphics grph = Graphics.FromImage(bitmap);
+
+            grph.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+            return bitmap;
+        }
+
+        public async Task<byte[]> ImageToByteAsync(System.Drawing.Image image)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Save the image to memoryStream in JPEG format
+                image.Save(memoryStream, ImageFormat.Jpeg);
+
+                // Convert the data in memoryStream to a byte array and return
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
